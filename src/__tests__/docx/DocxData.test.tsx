9af96591cf8d68d25a677bef39eebbf6ac1bb80f
Leonardo <mendes.lnr@gmail.com>
2022-07-24 15:21:13 -0300

    Updating docx and xlsx tests

--- src/__tests__/docx/DocxData.test.tsx
+++ src/__tests__/docx/DocxData.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import DocxData from '../../components/docx/DocxData';
 
 describe('DocxData component tests', () => {
@@ -8,5 +9,15 @@ describe('DocxData component tests', () => {
     render(<DocxData setTemplateFile={jest.fn()} />);
     const p = screen.getByText(labelText);
     expect(p).toBeInTheDocument();
+  });
+
+  test('DocxData accepts file upload', () => {
+    const file = new File(['mock'], 'mock.docx', { type: 'docx' });
+    render(<DocxData setTemplateFile={jest.fn()} />);
+    const input: HTMLInputElement = screen.getByLabelText(labelText);
+    userEvent.upload(input, file);
+    expect(input.files![0]).toStrictEqual(file);
+    expect(input.files!.item(0)).toStrictEqual(file);
+    expect(input.files).toHaveLength(1);
   });
 });
--- src/__tests__/xlsx/XlsxData.test.tsx
+++ src/__tests__/xlsx/XlsxData.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { defaultTemplate } from '../../App';
 import XlsxData from '../../components/xlsx/XlsxData';
 
@@ -16,22 +15,5 @@ describe('XlsxData component tests', () => {
     );
     const p = screen.getByText(labelText);
     expect(p).toBeInTheDocument();
-  });
-
-  test('XlsxData accepts file upload', () => {
-    const file = new File(['mock'], 'mock.xlsx', { type: 'xlsx' });
-    render(
-      <XlsxData
-        xlsxJson={[]}
-        setXlsxJson={jest.fn()}
-        templateData={defaultTemplate}
-        setTemplateCols={jest.fn()}
-      />
-    );
-    const input: HTMLInputElement = screen.getByLabelText(labelText);
-    userEvent.upload(input, file);
-    expect(input.files![0]).toStrictEqual(file);
-    expect(input.files!.item(0)).toStrictEqual(file);
-    expect(input.files).toHaveLength(1);
   });
 });
